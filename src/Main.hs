bcdee6788cf21bed18c848ac40dc0a57d4ce145c
IQubic <avi.caspe@gmail.com>
2017-04-11 13:10:54 -0700

    Created mandel function. Is a :+ b in the set?

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,14 @@
 module Main where
 
+import Data.Complex
+
 main :: IO ()
 main = do
   putStrLn "hello world"
+
+iter :: Int
+iter = 1000000
+
+mandel :: RealFloat a => a -> a -> Int
+mandel r i = length . takeWhile (\z -> magnitude z <= 2) .
+    take iter $ iterate (\z -> z^2 + (r :+ i)) 0
