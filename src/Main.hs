90d854f2588102b8c005c1e165e08f6f4160b243
IQubic <avi.caspe@gmail.com>
2017-04-11 18:05:56 -0700

    Added Functions to get a CNum's Escape Time

--- src/Main.hs
+++ src/Main.hs
@@ -6,9 +6,31 @@ main :: IO ()
 main = do
   putStrLn "hello world"
 
-iter :: Int
-iter = 1000000
+maxIter :: Int
+maxIter = 10000
 
+-- data Scale = {
+--                ULCorner :: Point
+--                DRCorner :: Point
+--                XScale :: Double
+--                YScale :: Double
+--              }
+
+-- Simple boolean test to see if a point is in the set
+isInSet :: RealFloat a => a -> a -> Bool
+isInSet r i = case escapeTime r i of
+              Nothing -> True
+              Just _ -> False
+
+-- Escape Time of Nothing means the point never escaped
+escapeTime :: RealFloat a => a -> a -> Maybe Int
+escapeTime r i
+    | numIter == maxIter = Nothing
+    | otherwise = Just numIter
+    where numIter = mandel r i
+
+-- Figures out how many iterations a point takes to escape
+-- Or stops after maxIter iterations
 mandel :: RealFloat a => a -> a -> Int
-mandel r i = length . takeWhile (\z -> magnitude z <= 2) .
-    take iter $ iterate (\z -> z^2 + (r :+ i)) 0
+mandel r i = length . takeWhile ((<= 2) . magnitude) .
+    take maxIter $ iterate (\z -> z^2 + (r :+ i)) 0
